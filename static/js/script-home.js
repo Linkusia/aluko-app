2887164da0fc5892f0991dd278de5010655eb596
Linkusia <paulina.mrozinska90@gmail.com>
2025-04-07 19:34:41 +0200

    poprawka 2 do menu

--- static/js/script-home.js
+++ static/js/script-home.js
@@ -66,9 +66,11 @@ window.onload = function () {
     yearSpan.textContent = new Date().getFullYear();
   }
   // Hamburger menu toggle
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-links");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-});
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-links");
+  
+  if (hamburger && navLinks) {
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("show");
+    });
+  }
